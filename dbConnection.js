d297533877d4b4e6435270a24e07bd39b0352487
Luiz Henrique Cudo <86842391+cudo80@users.noreply.github.com>
2021-09-24 14:50:45 -0300

    Update dbConnection.js

--- dbConnection.js
+++ dbConnection.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg')
 
 const client = new Pool({
-    connectionString: process.env.DATABASE_URL || 'postgres://aconmgdxqeqiak:840dee4c674360312329a7332dc8889204dc0b7c1cf4738413ced480a7859cc4@ec2-18-209-143-227.compute-1.amazonaws.com:5432/daglmgcbo4jka5',
+    connectionString: process.env.DATABASE_URL || '...',
     ssl:{
         rejectUnauthorized: false
     }
